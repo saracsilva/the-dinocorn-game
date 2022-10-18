a8be9d6e0e4b53c77bcd2ab62ef901e1825c7ce6
Sara Silva <saracarolinasilva@gmail.com>
2022-10-18 22:24:17 +0200

    Made the Jump

--- js/index.js
+++ js/index.js
@@ -5,6 +5,9 @@ const ctx = canvas.getContext(`2d`);
 const startScreen = document.querySelector(".start-screen");
 let keys = {};
 // Variables
+// key pressed
+let registerKeyPress = 0;
+let amountOfPressedAllowed = 5;
 // Game definitions
 const floor_01 = new Image();
 floor_01.src = "../img/floor.png";
@@ -12,6 +15,8 @@ const floor_02 = new Image();
 floor_02.src = "../img/floor.png";
 const background = new Image();
 background.src = "../img/background.png";
+const obstacle = new Image();
+obstacle.src = "../img/cactus_01.png";
 
 let isGameOver = false;
 let gameId = 0;
@@ -20,57 +25,44 @@ let gravity;
 let player;
 const dinoCorn = new Image();
 dinoCorn.src = "../img/dino_walk_01.png";
-
+let jumping = false;
 class Dinocorn {
   constructor() {
     this.dinoX = 50;
     this.dinoY = canvas.height - 180;
     this.dinoWidth = 100;
     this.dinoHeight = 160;
+    this.originalHeigh = this.dinoY;
     this.vY = 0;
-    //this.gravity = 0.5;
-    this.originalHeight = this.dinoHeight;
-    this.grounded = false;
-    this.jumpTimer = 0;
-  }
-  Animate() {
-    document.addEventListener("keydown", (event) => {
-      console.log(event);
-      if (event.code === "Space") {
-        console.log("We are going right!");
-        this.Jump();
-      }
-    });
-    document.addEventListener("keyup", () => {
-      this.jumpTime = 0;
-    });
+    this.gravity = 10;
 
-    this.dinoY += this.vY;
-    // gravity
-    if (this.dinoY + this.dinoHeight < canvas.height) {
-      this.vY += gravity;
-      this.grounded = false;
-    } else {
-      this.vY = 0;
-      this.grounded = true;
-      this.dinoY = canvas.height - this.dinoHeight;
-    }
+    this.jumpTimer = 0;
+    this.grounded = false;
+    this.jumpForce + 15;
   }
+
   Jump() {
-    //this.vY = -25;
-    if (this.grounded && this.jumpTimer == 0) {
-      this.jumpTimer = 1;
-      this.vY = -this.jumpForce;
-    } else if (this.jumpTimer > 0 && this.jumpTimer < 15) {
-      this.jumpTimer++;
-      this.vY = -this.jumpForce - this.jumpTimer / 50;
-    }
+    this.vY = -8;
+    console.log("yes");
   }
-  /* move() {
-    this.dinoY += this.vY;
+  Move() {
+    //this.dinoY = -canvas.height + 600;
+    if (this.dinoY + this.dinoHeight < canvas.height && jumping == false) {
+      this.dinoY += this.gravity;
+    }
+    if (this.dinoY < -canvas.height + 600) {
+      this.dinoY += this.gravity;
+      this.vY = 0;
+      jumping == false;
+    } else {
+      this.dinoY += this.vY;
+    }
+    /*  this.dinoY += this.vY;
     this.vY += this.gravity;
-    this.dinoY = constrain(this.dinoY, 0, canvas.height - 50);
-  } */
+    if (this.dinoY > canvas.height || this.dinoY < this.originalHeigh - 180) {
+      this.dinoY = this.originalHeigh;
+    } */
+  }
   Draw() {
     ctx.drawImage(
       dinoCorn,
@@ -81,16 +73,25 @@ class Dinocorn {
     );
   }
 }
-/* let dinocornX = 50;
-let dinocornY = canvas.height - 180;
-const dinoHeight = 160;
-const dinoWidth = 100; */
-// Dino Jump Definitions
-/* let jumpForce = 15;
-let dirY = 0;
-let grounded = false;
-let jumpTime = 0; */
-//let isJumping = false;
+// Obstacles
+class Obstacle {
+  constructor() {
+    this.x = 50;
+    this.y = canvas.height - 180;
+    this.width = 100;
+    this.height = 160;
+    this.dX = -gameSpeed;
+  }
+  Update() {
+    this.x += this.dX;
+    this.Draw;
+    this.dX = -gameSpeed;
+  }
+  Draw() {
+    ctx.drawImage(obstacle, this.x, this.y, this.width, this.height);
+  }
+}
+
 //floor definitions
 let floorW = 0;
 let floorW2 = canvas.width;
@@ -99,9 +100,9 @@ const animate = () => {
   ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
   ctx.drawImage(floor_01, floorW, canvas.height - 100, canvas.width + 50, 20);
   ctx.drawImage(floor_02, floorW2, canvas.height - 100, canvas.width + 50, 20);
-  //player.move();
+
   player.Draw();
-  player.Animate();
+  player.Move();
   console.log("test");
   // Make Dino Jump
 
@@ -121,16 +122,6 @@ const animate = () => {
     // Ask for a new frame
     gameId = requestAnimationFrame(animate);
   }
-  // Gravity
-  /* if (player.dinoY + player.dinoHeight < canvas.height) {
-    player.vY += gravity;
-    grounded = false;
-  } else {
-    player.vY = 0;
-    grounded = true;
-    player.dinoY = canvas.height - player.dinoHeight;
-  }
-  player.dinoY += player.vY; */
 };
 
 window.onload = () => {
@@ -141,8 +132,27 @@ window.onload = () => {
   };
   function startGame() {
     startScreen.style.display = "none";
-    gravity = 1;
+
     player = new Dinocorn();
     animate();
+    document.addEventListener("keydown", (event) => {
+      if (event.code === "Space") {
+        console.log("jump");
+        jumping = true;
+        registerKeyPress++;
+        player.Jump();
+        if (registerKeyPress >= 5) {
+          player.vY = 0;
+          jumping = false;
+          player.jumpTimer = 0;
+        }
+      }
+    });
+    document.addEventListener("keyup", () => {
+      player.vY = 0;
+      jumping = false;
+
+      registerKeyPress = 0;
+    });
   }
 };
