4f6d1de4e1b324fea740b99e3003921142bac51c
Sara Silva <saracarolinasilva@gmail.com>
2022-10-20 16:23:39 +0200

    more obstacles

--- js/index.js
+++ js/index.js
@@ -9,32 +9,51 @@ const restartScreen = document.querySelector(".restart-screen");
 let score = document.querySelector(".score");
 let scoreValue = 0;
 let highscore = document.querySelector(".highScore");
+/* function countRestartButton() {
+  let count = 0;
+  count += 1;
+  if (count > 0) {
+    player.vY = 0;
+  }
+} */
 
 // Variables
+//Dino image animation
+let frameDino = 0;
+setInterval(function () {
+  frameDino += 1;
+}, 120);
 // key pressed
-let registerKeyPress = 0;
-let amountOfPressedAllowed = 4;
+let registerKeyPress;
+let amountOfPressedAllowed = 5;
 // Game definitions
 const floor_01 = new Image();
-floor_01.src = "../img/floor.png";
+floor_01.src = "../img/floor_02.png";
 const floor_02 = new Image();
-floor_02.src = "../img/floor.png";
+floor_02.src = "../img/floor_02.png";
 const background = new Image();
 background.src = "../img/background.png";
 const obstacle_01 = new Image();
 obstacle_01.src = "../img/cactus_01.png";
 const obstacle_02 = new Image();
 obstacle_02.src = "../img/cactus_02.png";
-const dino_frame_count = 2;
-const frame_time = 100;
+const obstacle_03 = new Image();
+obstacle_03.src = "../img/obst_03.png";
+const obstacle_04 = new Image();
+obstacle_04.src = "../img/obst_04.png";
+const obstacle_05 = new Image();
+obstacle_05.src = "../img/obst_05.png";
+/* const dino_frame_count = 2;
+const frame_time = 100; */
 
 let isGameOver = false;
 let gameId = 0;
 let gravity;
-let gameSpeed;
+let gameSpeed = 3;
 let obstacles = [];
 
 // Dinocorn definitions
+let airTime = 0;
 let player;
 const dinoCorn_01 = new Image();
 dinoCorn_01.src = "../img/dino_walk_01.png";
@@ -42,6 +61,9 @@ const dinoCorn_02 = new Image();
 dinoCorn_02.src = "../img/dino_walk_02.png";
 const dinoCorn_jump = new Image();
 dinoCorn_jump.src = "../img/dino_jump.png";
+const dinoCorn_shrink = new Image();
+dinoCorn_shrink.src = "../img/dino_shrink.png";
+let isShrinking = false;
 
 let jumping = false;
 
@@ -51,42 +73,59 @@ class Dinocorn {
     this.dinoY = canvas.height - 180;
     this.dinoWidth = 100;
     this.dinoHeight = 160;
-    this.originalHeigh = this.dinoY;
+
     this.vY = 0;
     this.gravity = 8;
-
-    this.jumpTimer = 0;
-    this.grounded = false;
-    this.jumpForce + 15;
-    this.groundLimit = canvas.height - 180;
   }
 
   Jump() {
-    this.vY += -8;
+    this.vY = -10;
     jumping = true;
-
-    console.log("yes");
-    console.log(jumping);
+    /*  if (registerKeyPress >= amountOfPressedAllowed) {
+      //player.vY = 0;
+      jumping = false;
+      //console.log(this.vY, gameSpeed, jumping, "yes");
+    } */
   }
   Move() {
-    let timeinAir = 0;
     //this.dinoY = -canvas.height + 600;
     if (this.dinoY + this.dinoHeight < canvas.height && jumping == false) {
       this.dinoY += this.gravity;
+      //airTime++;
     }
     if (jumping === true) {
-      console.log("hello");
       this.dinoY += this.vY;
     } else if (this.dinoY + this.dinoHeight > 580 && jumping == false) {
       this.dinoY = canvas.height - 180;
       this.vY = 0;
     }
     if (this.dinoY < 100) {
+      this.dinoY += this.gravity;
+
+      setTimeout((this.vY = 0), 100);
+      //this.vY = 0;
       jumping = false;
     }
+
+    /*  if (airTime > 20) {
+      this.dinoY += this.gravity;
+      airTime = 0;
+    } */
+    /*  if (registerKeyPress > 2) {
+      this.dinoY += this.gravity;
+    } */
   }
+
   Draw() {
-    if (jumping) {
+    if (isShrinking) {
+      ctx.drawImage(
+        dinoCorn_shrink,
+        this.dinoX,
+        this.dinoY + 80,
+        this.dinoWidth + 20,
+        80
+      );
+    } else if (jumping) {
       ctx.drawImage(
         dinoCorn_jump,
         this.dinoX,
@@ -94,16 +133,16 @@ class Dinocorn {
         this.dinoWidth,
         this.dinoHeight
       );
-    } /* if (gameId % 2 === 0) */ else {
-      ctx.drawImage(
-        dinoCorn_01,
-        this.dinoX,
-        this.dinoY,
-        this.dinoWidth,
-        this.dinoHeight
-      );
-
-      setInterval(function () {
+    } else {
+      if (frameDino % 2 === 0) {
+        ctx.drawImage(
+          dinoCorn_01,
+          this.dinoX,
+          this.dinoY,
+          this.dinoWidth,
+          this.dinoHeight
+        );
+      } else {
         ctx.drawImage(
           dinoCorn_02,
           this.dinoX,
@@ -111,38 +150,25 @@ class Dinocorn {
           this.dinoWidth,
           this.dinoHeight
         );
-      }, 100);
-    } /*  else {
-      ctx.drawImage(
-        dinoCorn_02,
-        this.dinoX,
-        this.dinoY,
-        this.dinoWidth,
-        this.dinoHeight
-      );
-    } */
+      }
+    }
   }
 }
 
 // Obstacles
 
 class Obstacle {
-  constructor(type) {
-    this.x = 1500;
-    this.y = canvas.height - 180;
-    this.width = 100;
-    this.height = 160;
-    // this.dX = -gameSpeed;
+  constructor(x, y, width, height, type) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    //this.dX = -gameSpeed;
     this.type = type;
   }
-  /* Update() {
-    this.x += this.dX;
-    this.Draw;
-    this.dX = -gameSpeed;
-  } */
+
   Draw() {
     ctx.drawImage(this.type, this.x, this.y, this.width, this.height);
-    //ctx.drawImage(obstacle_02, this.x, this.y, this.width, this.height);
   }
 }
 
@@ -150,102 +176,148 @@ function RandomObstRange(min, max) {
   return Math.round(Math.random() * (max - min) + min);
 }
 // Obstacles Creation definition
-/* let inicialTimeOb = 200;
-let intervalTimeOb = 200; */
-let obstaclesFrequency = 0;
+
+let obstaclesFrequency = 500;
 //floor definitions
 let floorW = 0;
-let floorW2 = canvas.width;
+let floorW2 = canvas.width + 498;
 
 function detectCollision(obstacle) {
-  return (
-    player.dinoX + player.dinoWidth - 20 > obstacle.x &&
-    player.dinoY + player.dinoHeight > obstacle.y &&
-    player.dinoX < obstacle.x + obstacle.width - 50
-  );
+  if (isShrinking) {
+    return (
+      player.dinoY + 80 < obstacle.y + obstacle.height &&
+      player.dinoY + player.dinoHeight > obstacle.y &&
+      player.dinoX + player.dinoWidth - 20 > obstacle.x &&
+      player.dinoX < obstacle.x + obstacle.width - 50
+    );
+  } else {
+    return (
+      player.dinoY < obstacle.y + obstacle.height &&
+      player.dinoY + player.dinoHeight > obstacle.y &&
+      player.dinoX + player.dinoWidth - 20 > obstacle.x &&
+      player.dinoX < obstacle.x + obstacle.width - 50
+    );
+  }
 }
 const animate = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-  ctx.drawImage(floor_01, floorW, canvas.height - 100, canvas.width + 50, 20);
-  ctx.drawImage(floor_02, floorW2, canvas.height - 100, canvas.width + 50, 20);
+  ctx.drawImage(floor_01, floorW, canvas.height - 500, canvas.width + 500, 440);
+  ctx.drawImage(
+    floor_02,
+    floorW2,
+    canvas.height - 500,
+    canvas.width + 500,
+    440
+  );
 
   player.Draw();
   player.Move();
 
-  console.log("test");
-
-  obstaclesFrequency++;
-
   if (gameId % 500 == 0) {
     //Draw an obstacle
+    // Make obstacles
+    let type = RandomObstRange(0, 4);
+    let x;
+    let y;
+    let width;
+    let height;
 
-    let type = RandomObstRange(0, 1);
     if (type === 0) {
       type = obstacle_01;
+      x = 1900;
+      y = canvas.height - 180;
+      width = 100;
+      height = 160;
     } else if (type === 1) {
       type = obstacle_02;
+      x = 1900;
+      y = canvas.height - 180;
+      width = 100;
+      height = 160;
+    } else if (type === 2) {
+      type = obstacle_03;
+      x = 1900;
+      y = canvas.height - 200;
+      width = 80;
+      height = 70;
+    } else if (type === 3) {
+      type = obstacle_04;
+      x = 1900;
+      y = canvas.height - 100;
+      width = 100;
+      height = 80;
+    } else if (type === 4) {
+      type = obstacle_05;
+      x = 1900;
+      y = canvas.height - 180;
+      width = 100;
+      height = 160;
     }
-    let newObstacle = new Obstacle(type);
+    let newObstacle = new Obstacle(x, y, width, height, type);
     obstacles.push(newObstacle);
-    console.log(obstacles);
   }
   for (let i = 0; i < obstacles.length; i++) {
-    obstacles[i].x -= 6;
+    obstacles[i].x -= gameSpeed;
     obstacles[i].Draw();
 
     if (detectCollision(obstacles[i])) {
-      alert("BOOOOOMM!");
       isGameOver = true;
-      obstaclesFrequency = 0;
-
-      obstacles = [];
-      player = null;
-      scoreValue = 0;
-      score.innerHTML = 0;
     }
   }
-  // Make obstacles
 
   // Make the floor move
 
   floorW -= 3;
   floorW2 -= 3;
-  if (floorW < -canvas.width) {
-    floorW = canvas.width;
+  if (floorW < -canvas.width - 500) {
+    floorW = canvas.width + 498;
   }
-  if (floorW2 < -canvas.width) {
-    floorW2 = canvas.width;
+  if (floorW2 < -canvas.width - 500) {
+    floorW2 = canvas.width + 498;
   }
   //request animation
   if (isGameOver) {
     cancelAnimationFrame(gameId);
     restartScreen.classList.remove(`hidden`);
     gameScreen.classList.add("hidden");
+    obstaclesFrequency = 0;
+    //player.vY = 0;
+    obstacles = [];
+    // player = null;
+    scoreValue = 0;
+    score.innerHTML = 0;
   } else {
     // Ask for a new frame
     gameId = requestAnimationFrame(animate);
   }
   scoreValue++;
-
   score.innerHTML = scoreValue;
   if (scoreValue > highscore.innerText) {
     highscore.innerHTML = scoreValue;
   }
-  console.log(highscore.innerHTML);
-  //scoreValue = Number(score.innerText);
 };
 
 window.onload = () => {
   document.getElementById("start-button").onclick = () => {
-    console.log("starting");
     //song.play()
     startGame();
   };
+
   document.getElementById("restart-button").onclick = () => {
-    console.log("restarting");
     //song.play()
+    //countRestartButton();
+    obstacles = [];
+
+    registerKeyPress = 0;
+    console.log(amountOfPressedAllowed);
+    // player = null;
+    scoreValue = 0;
+    score.innerHTML = 0;
+    jumping = false;
     isGameOver = false;
+    gameSpeed = 3;
+    player.vY = 0;
 
     startGame();
   };
@@ -253,33 +325,25 @@ window.onload = () => {
     restartScreen.classList.add("hidden");
     startScreen.style.display = "none";
     gameScreen.classList.remove("hidden");
-    //gameSpeed = 3;
+    registerKeyPress = 0;
+    setInterval(function () {
+      gameSpeed += 0.05;
+    }, 2000);
 
-    //highscore = 0;
-
-    /* scoreText = new Text("Score: " + score, 25, 25, "left", "#212121", "20");
-    highscoreText = new Text(
-      "Highscore: " + highscore,
-      canvas.width - 25,
-      25,
-      "right",
-      "#212121",
-      "20"
-    ); */
     player = new Dinocorn();
+    player.vY = 0;
     animate();
 
     document.addEventListener("keydown", (event) => {
       if (event.code === "Space") {
-        console.log("jump");
         jumping = true;
-        registerKeyPress++;
+        registerKeyPress += 1;
 
         player.Jump();
         if (registerKeyPress >= amountOfPressedAllowed) {
           player.vY = 0;
           jumping = false;
-          player.jumpTimer = 0;
+          registerKeyPress = 0;
         }
       }
     });
@@ -289,6 +353,14 @@ window.onload = () => {
       jumping = false;
 
       setTimeout(() => (registerKeyPress = 0), "300");
+    });
+    document.addEventListener("keydown", (event) => {
+      if (event.code === "ArrowDown") {
+        isShrinking = true;
+      }
+    });
+    document.addEventListener("keyup", () => {
+      isShrinking = false;
     });
   }
 };
